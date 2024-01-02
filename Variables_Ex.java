5b956c4d960d562bd9cf1da0539ff606de05d140
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2024-01-02 09:17:36 +0530

    02-01-2024 day-3

---
+++ Variables_Ex.java
@@ -0,0 +1,19 @@
+class Variables_Ex  //PascalCase
+{	
+	//global variables -->instance,static
+	boolean result=true;
+	char gender='m';
+	public static void main(String args[])
+	{
+		//local variables
+		
+		byte age=31;
+		long contact=9494949494l;
+		float salary=23.4538234682349f;
+		double scientific=23.4538234682349;
+		
+		
+		System.out.println(age+" "+contact+" "+salary+" "+scientific);
+	}
+	
+}  
