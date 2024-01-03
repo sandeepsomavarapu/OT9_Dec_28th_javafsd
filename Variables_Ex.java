8640b74394b1e1208bd749b6da7c8d271e7c025c
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2024-01-03 09:07:48 +0530

    03-01-2023

--- Variables_Ex.java
+++ Variables_Ex.java
@@ -1,8 +1,9 @@
 class Variables_Ex  //PascalCase
 {	
 	//global variables -->instance,static
-	boolean result=true;
-	char gender='m';
+	boolean result=true;// 1 bit
+	char gender='m';   //2 bytes global variables -->instance
+	static int countryCode=91;//global variable -->static
 	public static void main(String args[])
 	{
 		//local variables
@@ -12,8 +13,15 @@ class Variables_Ex  //PascalCase
 		float salary=23.4538234682349f;
 		double scientific=23.4538234682349;
 		
-		
+		System.out.println(Variables_Ex.countryCode);//using static  variable by using classname	
 		System.out.println(age+" "+contact+" "+salary+" "+scientific);
+		
+		Variables_Ex obj=new Variables_Ex();//object creation
+		
+		System.out.println(obj.result);//using instance variable by using objref
+
+		
+						
 	}
 	
 }  
